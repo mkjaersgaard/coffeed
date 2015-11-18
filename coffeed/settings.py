c9626acca6d0c4dc88b514f9970e6ba8ecb83065
Michael Kjærsgaard <Mkjaersgaard@Michaels-MacBook-Air.local>
2015-11-18 20:56:39 +0100

    update tempaltes and settings

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -109,10 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Static/'
 
 STATICFILES_DIRS = (
-    os.path.join(MAIN_DIR, 'static'),
+    os.path.join(MAIN_DIR, 'Static'),
     )
 
 STATIC_ROOT = 'staticfiles' 
